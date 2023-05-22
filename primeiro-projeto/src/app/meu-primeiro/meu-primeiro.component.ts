8094494ca5d7a1143edf927fb12633dd77aec8fb
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-22 10:22:08 -0300

    curso angular #03: hello world

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>Meu primeiro component</p>
+  `
+})
+export class MeuPrimeiroComponent {}
