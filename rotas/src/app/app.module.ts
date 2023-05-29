a33cb2d2354001de1f51b1d3bdd90b20b14b8204
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-29 13:56:34 -0300

    #63: Rotas: Tela de Login e como não mostrar o Menu (NavBar)

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { LoginComponent } from './login/login.component';
 // import { CursosService } from './cursos/cursos.service';
 // import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { AppRoutingModule } from './app.routing.module';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './login/auth.service';
 // import { CursosModule } from './cursos/cursos.module';
 // import { AlunosModule } from './alunos/alunos.module';
 // import { AlunosComponent } from './alunos/alunos.component';
@@ -26,11 +28,13 @@ import { AppRoutingModule } from './app.routing.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     // CursosModule,
     // AlunosModule,
     AppRoutingModule
   ],
   // providers: [CursosService],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- rotas/src/app/login/login.component.ts
+++ rotas/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  usuario: Usuario = new Usuario()
+
+  constructor(private authService: AuthService) {}
+
+  fazerLogin(){
+    // console.log(this.usuario)
+    this.authService.fazerLogin(this.usuario)
+  }
 
 }
