5006e3ab2c424c98e91fbefcfa777668e594a697
pereirathiago <pereira.tp.thiago@gmail.com>
2023-05-30 13:51:27 -0300

    #74: Forms (template driven) Controles ngForm, ngSubmit e ngModel

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class TemplateFormComponent {
 
+
+  onSubmit(form: NgForm) {
+    console.log(form)
+  }
 }
